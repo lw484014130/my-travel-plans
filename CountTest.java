2ccdf0834b794257d4ebe4e99ee42c1947e72b8a
LW <484014130@qq.com>
2020-06-07 20:48:51 +0800

    JUnit

---
+++ CountTest.java
@@ -0,0 +1,28 @@
+package sizeyunsuan;
+
+import static org.junit.Assert.*;
+
+import java.util.Stack;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CountTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCount() {
+		Stack<String> z = new Stack<String>();
+		int num1=2,num2=3,d;
+		z.push(String.valueOf(5));
+	}
+
+}
---
+++ JudgeTest.java
@@ -0,0 +1,37 @@
+package sizeyunsuan;
+
+import static org.junit.Assert.*;
+
+import java.util.Scanner;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JudgeTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testA() {
+		 int A=10;
+		 Scanner scanner = new Scanner(System.in);
+		 int n=10;
+		 System.out.println("正确！");
+	}
+
+	@Test
+	public void testJudge() {
+		 int A=10;
+		 Scanner scanner = new Scanner(System.in);
+		 int n=10;
+		 System.out.println("正确！");
+	}
+
+}
